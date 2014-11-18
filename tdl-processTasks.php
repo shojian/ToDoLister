9152236b931a845b81f8e99bff6df001627cba73
Sarah Gebauer <sarah@blogonapaper.com>
2014-11-18 22:13:48 +0100

    Dutch commit 2
    Prepared removing tasks from DB

--- tdl-processTasks.php
+++ tdl-processTasks.php
@@ -21,7 +21,8 @@ if (isset($_GET["add"])) {
 	for ($i = 0; $i < count($toProcess); $i++) {
 		if (strpos($toProcess[$i], "#") == 0) {
 			// labels			
-			$labels[] = substr($toProcess[$i], 1);
+			if (!is_numeric(substr($toProcess[$i], 1))) // allowing to write "I'm #1"
+				$labels[] = substr($toProcess[$i], 1);
 		} else if ((strpos($toProcess[$i], "@") == 0) && (empty($project))) {
 			// project
 			$project = substr($toProcess[$i], 1);
@@ -46,11 +47,18 @@ if (isset($_GET["add"])) {
 if (isset($_GET["done"])) {
 	/*
 	 *  Transfer from To Be Done to Done table
-	 *  Optionally 
+	 *  Optionally renewing task
 	 */
 }
 
-if (isset($_GET["remove"]) && isset($_POST["id"])) {
-
+if (isset($_GET["remove"]) && isset($_POST["toBeRemoved"])) {
+	/*
+	 *  id cannot be post. It can in theory but just to be on safe side POST is ok, DELETE would be better
+	 */
+	 if ($stmt = $mysqli->prepare("DELETE FROM TASKS WHERE id=? LIMIT 1")) {
+	 	$stmt->bind_param("i", $_POST["toBeRemoved"]);
+	 	$stmt->execute();
+	 	$stmt->close();
+	 }
 }
 ?>
